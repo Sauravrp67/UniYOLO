7580397efa231b64d3f0ea8f66fcfd2b405ea2b1
Sauravrp67 <paudelsaurav59@gmail.com>
2025-09-07 00:57:19 +0545

    Completed YOLOv3 Pruning Script

--- VainF_pruning/pruning_utils.py
+++ VainF_pruning/pruning_utils.py
@@ -1,6 +1,23 @@
 import torch.nn as nn
 import torch
 import torch_pruning as tp
+import json
+from tqdm import tqdm
+import numpy as np
+from pathlib import Path
+import sys
+
+
+ROOT = Path(__file__).resolve().parents[1]
+
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+
+from model import YOLOv3_DPU,YOLOv3,BoxDecoder,do_sigmoid
+from dataloader import Dataset,BasicTransform,AugmentTransform
+from torch.utils.data import DataLoader,Subset
+from utils import YOLOv3Loss,scale_coords,transform_xcycwh_to_x1y1x2y2,transform_x1y1x2y2_to_x1y1wh,filter_confidence,run_NMS,to_image
+
 
 def count_params(m):
     return sum(p.numel() for p in m.parameters())
@@ -236,3 +253,126 @@ def bn_recalibration(model, loader, iters=400, device="cpu", only_bn_train=True,
         if isinstance(m, nn.BatchNorm2d):
             m.momentum = orig_mom[i]; i += 1
     model.eval()
+
+@torch.no_grad()
+def validate(args,anchors, dataloader, model, evaluator, dpu:bool = False,save_result=False,save_filename:str = "predictions.txt"):
+    model.eval()
+    if not dpu :
+        model.module.set_grid_xy(input_size=args.img_size) if hasattr(model, "module") else model.set_grid_xy(input_size=args.img_size)
+
+    with open(args.mAP_filepath, mode="r") as f:
+        mAP_json = json.load(f)
+
+    cocoPred = []
+    check_images, check_preds, check_results = [], [], []
+    imageToid = mAP_json["imageToid"]
+
+    for _, minibatch in enumerate(dataloader):
+        filenames, images, shapes = minibatch[0], minibatch[1], minibatch[3]
+        predictions = model(images.to("cpu"))
+        if dpu:
+            decoded_52 = BoxDecoder(predictions[0],torch.tensor(anchors[0:3])).decode_predictions()
+            decoded_26 = BoxDecoder(predictions[1],torch.tensor(anchors[3:6])).decode_predictions()
+            decoded_13 = BoxDecoder(predictions[2],torch.tensor(anchors[6:9])).decode_predictions()
+            predictions = torch.cat((decoded_52,decoded_26,decoded_13),dim = 1)
+        # cuda(args.rank, non_blocking=True)
+
+        for j in range(len(filenames)):
+            prediction = predictions[j].cpu().numpy()
+
+            prediction[:, 1:5] = transform_xcycwh_to_x1y1x2y2(boxes=prediction[:, 1:5], clip_max=1.0)
+            prediction = filter_confidence(prediction=prediction, conf_threshold=args.conf_thres)
+            prediction = run_NMS(prediction=prediction, iou_threshold=args.nms_thres)
+
+            if len(check_images) < 5:
+                check_images.append(to_image(images[j]))
+                check_preds.append(prediction.copy())
+                
+            if len(prediction) > 0:
+                filename = filenames[j]
+                shape = shapes[j]
+                cls_id = prediction[:, [0]]
+                conf = prediction[:, [-1]]
+                box_x1y1x2y2 = scale_coords(img1_shape=images.shape[2:], coords=prediction[:, 1:5], img0_shape=shape[:2])
+                box_x1y1wh = transform_x1y1x2y2_to_x1y1wh(boxes=box_x1y1x2y2)
+                img_id = np.array((imageToid[filename],) * len(cls_id))[:, np.newaxis]
+                cocoPred.append(np.concatenate((img_id, box_x1y1wh, conf, cls_id), axis=1))
+
+    del images, predictions
+    torch.cuda.empty_cache()
+
+    if len(cocoPred) > 0:
+        cocoPred = np.concatenate(cocoPred, axis=0)
+        mAP_dict, eval_text = evaluator(predictions=cocoPred)
+
+        if save_result:
+            np.savetxt(args.exp_path / save_filename, cocoPred, fmt="%.4f", delimiter=",", header=f"Inference results of [image_id, x1y1wh, score, label]") 
+            out_file = args.exp_path / save_filename
+            with open(out_file, "a") as f:
+                f.write(eval_text)
+        return mAP_dict, eval_text
+    else:
+        return None, None
+
+def get_dataloader(voc_path,batch_size,same_subset = False, subset_length = 8,train = False,input_size = 416,mAP_filename = "eval.json"):
+    if not train:
+        val_dataset = Dataset(voc_path,phase = 'val')
+        if same_subset:
+            k = len(val_dataset)
+        else:
+            k = subset_length
+        print(len(val_dataset))
+        subset_index = list(range(min(k,len(val_dataset))))
+        val_transform = BasicTransform(input_size = input_size)
+        val_dataset.load_transformer(val_transform)
+        val_subset = Subset(val_dataset,indices = subset_index)
+        idx = list(val_subset.indices)
+        val_subset.dataset.generate_mAP_source(save_dir = Path("./data/eval_src"),mAP_filename = mAP_filename,indices = idx)
+        
+        return DataLoader(dataset=val_subset, collate_fn=Dataset.collate_fn, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=1,drop_last=True)
+    
+    else:
+        train_dataset = Dataset(voc_path,phase = "train")
+        if same_subset:
+            k = len(train_dataset)
+        else:
+            k = subset_length
+        subset_index = list(range(min(k,len(train_dataset))))
+        train_transform = AugmentTransform(input_size = input_size,dataset = train_dataset)
+        train_dataset.load_transformer(train_transform)
+        train_subset = Subset(train_dataset,indices = subset_index)
+        return DataLoader(dataset=train_subset, collate_fn=Dataset.collate_fn, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=1,drop_last=True)
+    
+
+def load_model(mode:str,device: str,input_size: int, num_classes: int,model_type: str,anchors,model_path:str):
+    """
+    Initialize YOLO model and load checkpoint.
+    mode:"normal" -> uninspected model,some layers might not be supported in the dpu
+          "dpu" -> dpu supported model
+          "dpu_quantized" -> Quantized Model in DPU.
+         
+    """
+    if mode == "dpu":
+        model = YOLOv3_DPU(input_size = input_size, num_classes = num_classes, anchors = anchors, model_type = model_type, pretrained = False).to(device)
+        ckpt = torch.load(model_path,map_location = 'cpu',weights_only = False)
+        sd = ckpt["model_state"] if "model_state" in ckpt else ckpt
+        missing,unexpected = model.load_state_dict(sd,strict = True)
+        model.set_grid_xy(input_size = input_size)
+        print(f"[load] missing = {len(missing)} unexpected = {len(unexpected)}")
+    
+    elif mode == "normal":
+        model = YOLOv3(input_size = input_size, num_classes = num_classes, anchors = anchors, model_type = model_type, pretrained = False).to(device)
+        ckpt = torch.load(model_path,map_location = 'cpu',weights_only = False)
+        sd = ckpt["model_state"] if "model_state" in ckpt else ckpt
+        missing,unexpected = model.load_state_dict(sd,strict = True)
+        model.set_grid_xy(input_size = input_size)
+        print(f"[load] missing = {len(missing)} unexpected = {len(unexpected)}")
+    elif mode == "dpu_quantized":
+        model = torch.jit.load(model_path,map_location=device)
+    
+    model.to(device).eval()
+    model.zero_grad() 
+    return model
+
+
+
--- VainF_pruning/yolov3pruning.py
+++ VainF_pruning/yolov3pruning.py
@@ -1,20 +1,28 @@
 import torch
 import torch_pruning as tp
-from pruning_utils import collect_ignored_convs,validate,load_model
 from typing import Tuple, Optional
-from pathlib import Path
-import sys
 import torch 
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.cuda import amp
+from torch import optim
+import torch.distributed as dist
+import torch.multiprocessing as mp
 from tqdm import tqdm
+from pathlib import Path
+import sys
+import os
+import argparse
+from typing import Tuple, Optional
+from pruning_utils import collect_ignored_convs,validate,load_model,get_dataloader
+
 
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
-from utils import YOLOv3Loss
-
+from utils import YOLOv3Loss,Evaluator,set_lr
+from model import do_sigmoid
 
 
 class AverageMeter:
@@ -33,19 +41,85 @@ class AverageMeter:
 
 def train_one_epoch(model, loader, criterion, optimizer, device):
     model.train()
-    loss_meter = AverageMeter()
+    multipart_loss_meter = AverageMeter()
+    obj_loss_meter = AverageMeter()
+    noobj_loss_meter = AverageMeter()
+    txty_loss_meter = AverageMeter()
+    twth_loss_meter = AverageMeter()
+    cls_loss_meter = AverageMeter()
+    optimizer.zero_grad()
 
-    for images, targets in tqdm(loader, desc="Training", leave=False):
+    for (_,images, targets,_) in tqdm(loader, desc="Training", leave=False):
+
         images = images.to(device, non_blocking=True)
-        targets = targets.to(device, non_blocking=True)
-
+        
         optimizer.zero_grad(set_to_none=True)
+        
         outputs = model(images)
         loss = criterion(outputs, targets)
-        loss.backward()
+
+        loss[0].backward()
         optimizer.step()
 
-    return loss_meter.avg
+        multipart_loss_meter.update(loss[0].item(),images.size(0))
+        obj_loss_meter.update(loss[1].item(),images.size(0))
+        noobj_loss_meter.update(loss[2].item(),images.size(0))
+        txty_loss_meter.update(loss[3].item(),images.size(0))
+        twth_loss_meter.update(loss[4].item(),images.size(0))
+        cls_loss_meter.update(loss[5].item(),images.size(0))
+
+    return [multipart_loss_meter.avg,obj_loss_meter.avg,noobj_loss_meter.avg,txty_loss_meter.avg,twth_loss_meter.avg,cls_loss_meter.avg]
+
+@torch.no_grad()
+def calc_mAP(args,model,val_loader,anchors,dpu:bool = True):
+    model.eval()
+    subset_class = val_loader.dataset
+    base_dataset_class = val_loader.dataset.dataset
+    idx = list(subset_class.indices)
+
+    base_dataset_class.generate_mAP_source(save_dir = Path("./data/eval_src"),mAP_filename =mAP_filename,indices = idx)
+    args.mAP_filepath = Path(base_dataset_class.mAP_filepath)
+    args.exp_path = Path(args.exp_path)
+
+    os.makedirs(args.exp_path, exist_ok=True)
+
+    evaluator = Evaluator(args.mAP_filepath)
+
+    mAP_dict,eval_text = validate(args,anchors = anchors, dataloader = val_loader,model = model,evaluator = evaluator,save_result = True,dpu  = True, save_filename = "Pruned_map.txt")
+
+    return mAP_dict,eval_text
+
+@torch.no_grad()
+def evaluate(args, model, loader, criterion,anchors, device, desc="eval"):
+    model.eval()
+    multipart_loss_meter = AverageMeter()
+    obj_loss_meter = AverageMeter()
+    noobj_loss_meter = AverageMeter()
+    txty_loss_meter = AverageMeter()
+    twth_loss_meter = AverageMeter()
+    cls_loss_meter = AverageMeter()
+
+    for _,images, targets,_ in tqdm(loader, desc=f"Evaluating-{desc}", leave=False):
+        images = images.to(device, non_blocking=True)
+        out = model(images)
+        preds0 = do_sigmoid(out[0])
+        preds1 = do_sigmoid(out[1])
+        preds2 = do_sigmoid(out[2])
+        outputs = (preds0,preds1,preds2)
+
+        loss = criterion(outputs, targets)
+        multipart_loss_meter.update(loss[0].item(),images.size(0))
+        obj_loss_meter.update(loss[1].item(),images.size(0))
+        noobj_loss_meter.update(loss[2],images.size(0))
+        txty_loss_meter.update(loss[3],images.size(0))
+        twth_loss_meter.update(loss[4],images.size(0))
+        cls_loss_meter.update(loss[5],images.size(0))
+        # loss_meter.update(loss, images.size(0))
+
+    mAP_dict,eval_text = calc_mAP(args,model,val_loader = loader,anchors = anchors,dpu = True)
+    print(f"[{desc}]\nMultipart_Loss: {multipart_loss_meter.avg:.4f} | Object Loss:{obj_loss_meter.avg:.4f} | No Object Loss:{noobj_loss_meter.avg:.4f} | txty Loss:{txty_loss_meter.avg:.4f} twth Loss:{twth_loss_meter.avg:.4f} | cls loss:{cls_loss_meter.avg:.4f}\nmAP:{eval_text}")
+    return [multipart_loss_meter.avg,obj_loss_meter.avg,noobj_loss_meter.avg,txty_loss_meter.avg,twth_loss_meter.avg,cls_loss_meter.avg],mAP_dict
+
 
 def taylor_prune(model: nn.Module,
                  example_inputs: torch.Tensor,
@@ -66,11 +140,43 @@ def taylor_prune(model: nn.Module,
     # Importance: TaylorExpansion (requires gradients)
     imp = tp.importance.TaylorImportance()
 
-    # Ignore classifier
-    # if ignored_layers is None:
-    #     ignore_layers = 
+    pruner = tp.pruner.MagnitudePruner(
+        model,
+        example_inputs.to(device),
+        importance = imp,
+        iterative_steps = iter_steps,
+        pruning_ratio = pruning_ratio,
+        ignored_layers = ignored_layers,
+        round_to = round_to
+    )
+
+    base_macs,base_params = tp.utils.count_ops_and_params(model,example_inputs.to(device))
+    print(f"[Pruning] Baseline: MACs={base_macs/1e6:.2f}M | Params={base_params/1e6:.2f}M")
+
+    optimizer = optim.SGD(model.parameters(),lr = lr,momentum = 0.9, weight_decay = 1e-4)
+
+    for i in range(iter_steps):
+        _,images,targets,_ = next(iter(train_loader))
+        images = images.to(device)
+        
+        optimizer.zero_grad(set_to_none = True)
+        outputs = model(images)
+        loss = criterion(outputs,targets)
+        loss[0].backward()
+        
+        pruner.step()
+        macs, params = tp.utils.count_ops_and_params(model, example_inputs.to(device))
+        print(f"[Pruning] Step {i+1}/{iter_steps}: MACs={macs/1e6:.2f}M | Params={params/1e6:.2f}M")
+        if finetune_epochs > 0:
+            for e in range(finetune_epochs):
+                tr_loss = train_one_epoch(model, train_loader, criterion, optimizer, device)
+                print(f"↳ fine-tune e{e+1}/{finetune_epochs} | loss={tr_loss}")
+        
+    return model
+
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     anchors = [
         [0.248,      0.7237237 ],
         [0.36144578, 0.53      ],
@@ -82,7 +188,71 @@ if __name__ == "__main__":
         [0.8605263,  0.8736842 ],
         [0.944,      0.5733333 ]
         ]
+
+    parser = argparse.ArgumentParser(description = "YOLOv3 Pruning")
+    parser.add_argument('--img-size',
+                        type = int,
+                        default = 416,
+                        help = "Required Image Size to the model")
+    parser.add_argument('--exp-path',
+                        type = str,
+                        default = './mAP_results')
+    parser.add_argument('--conf-thres',
+                        type = float,
+                        default = 0.3,
+                        help = "confidence threshold for calculating mAP")
+    parser.add_argument('--nms-thres',
+                        type = float,
+                        default = 0.6,
+                        help = "nms threshold for calculating mAP")
+    parser.add_argument('--base-lr', 
+                        type = float,
+                        default = 0.001, 
+                        help = "Base Learning rate"
+                    )
+    parser.add_argument('--momentum',
+                        type = float,
+                        default = 0.9,
+                        help = "momentum for optimizer")
+    parser.add_argument("--lr-decay", 
+    nargs="+", 
+    default=[150, 200], 
+    type=int, 
+    help="Epoch to learning rate decay"
+    )
+
+    parser.add_argument('--weight-decay',
+                        type = float,
+                        default = 0.0005,
+                        help = 'Weight Decay')
+
+
+
+    args,_ = parser.parse_known_args()
+    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model(mode = "dpu",input_size = 416,num_classes = 20,model_type = "base",anchors = anchors,device = device,model_path = '/home/logictronix01/saurav/YOLOv3/weights/yolov3-base.pt')
-    ignored_layers = collect_ignored_convs(model)
-    print(model)
+    
+    model = load_model(mode = "dpu",input_size = 416,num_classes = 20,model_type = "base",anchors = anchors,device = device,model_path = '/home/logictronix01/saurav/YOLOv3/weights/yolov3-base.pt').to(device)
+    ignored_layers = collect_ignored_convs(model,keep_stem = False,keep_stage_entry = False,)    
+
+    train_loader = get_dataloader(voc_path = '/home/logictronix01/saurav/YOLOv3/data/voc.yaml',batch_size = 8,same_subset = False, subset_length = 2000, train = True)
+    test_loader = get_dataloader(voc_path = '/home/logictronix01/saurav/YOLOv3/data/voc.yaml',batch_size = 8,same_subset = False,subset_length = 100, train = False)
+
+    #Unit Test: evaluate function
+    criterion = YOLOv3Loss(input_size=416,num_classes = 20,anchors = model.anchors)    
+    loss,mAP_dict = evaluate(args = args,model = model,loader = test_loader,criterion=criterion,anchors = model.anchors,device = device,desc = "eval")
+    print(loss)
+    print(mAP_dict['all']['mAP_50'])
+
+    # Unit test: train_one_epoch function
+    optimizer = optim.SGD(model.parameters(),lr = args.base_lr,momentum = args.momentum,weight_decay = args.weight_decay)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer,milestones = args.lr_decay,gamma = 0.1)
+
+    # loss = train_one_epoch(model,train_loader,criterion,optimizer,device)
+    # print(loss)
+    example_inputs = torch.randn(1,3,416,416).to(device)
+    model = taylor_prune(model,example_inputs,train_loader,criterion,device=device,ignored_layers = ignored_layers,finetune_epochs = 3,round_to = 8)
+        
+    loss,mAP_dict = evaluate(args = args,model = model,loader = test_loader,criterion=criterion,anchors = model.anchors,device = device,desc = "eval")
+    print(loss)
+    print(mAP_dict['all']['mAP_50'])
